db4b6826ec7d9e0f709ae1871030be1d05563c1c
Serhat Demir <Serhatdmrw@gmail.com>
2023-04-03 14:43:08 +0300

    Initial Commit

---
+++ SimpsonBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBookProject
+//
+//  Created by Serhat Demir on 3.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
